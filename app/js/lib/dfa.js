c052858f5caa5c1ae1359d26e2b95a82f2610f79
Raphael Esterle <raphael.esterle@gmail.com>
2015-12-01 13:45:53 +0100

    Table representation of DEA

--- app/js/lib/dfa.js
+++ app/js/lib/dfa.js
@@ -26,10 +26,11 @@ var DFA = function(config){
   self.step = function(){
     if(self.status == 'stoped'){
       self.reset();
-      self.status = 'step'
-    } else if (self.status == undefined) {
+    } else if (_.include([undefined, 'accepted','not accepted'], self.status)) {
       return;
     }
+
+    self.status = 'step';
 
     var nextChar = self.input[self.count++];
     var nextState = _.filter(self.config.transitions, function(transition){
@@ -37,18 +38,21 @@ var DFA = function(config){
         self.status = 'not accepted';
         return;
       }
-
       return transition[0] == _.last(self.statusSequence) && transition[1] == nextChar;   
     });
-    
-    if (_.isEmpty(nextState)) {
+    if(_.isEmpty(nextState)){
       self.status = 'not accepted';
       return;
     }
-
+    
     var newStatus = nextState[0][2];
-    if((_.include(self.config.finalStates, newStatus)) && (self.input.length == self.count)){
-      self.status = 'accepted';
+    if(self.input.length == self.count){ 
+      if(_.include(self.config.finalStates, newStatus)){
+        self.status = 'accepted';
+      }
+      else{ 
+        self.status = 'not accepted';
+      }
     }
     self.statusSequence.push(newStatus);
     return nextState;
@@ -71,14 +75,15 @@ var DFA = function(config){
     if (!(_.include(['step', 'accepted', 'not accepted'], self.status))){
       return;
     }
+    self.status = 'step';
     
-    // Decrease count and remove last element from statusSequence
-    self.count--;
-    self.statusSequence.pop();
-
     // Reset if no more undoing is impossible
     if(self.count == 0){
       self.reset();
+    }else{ 
+      // Decrease count and remove last element from statusSequence
+      self.count--;
+      self.statusSequence.pop();
     }
   }
 
