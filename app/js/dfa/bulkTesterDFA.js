38c2c43489309588c41b5c7e43bf461392932e53
MRisto <risto.matthias@gmail.com>
2016-08-18 02:00:10 +0200

    bulktester needs design polish, then its finished

--- app/js/dfa/bulkTesterDFA.js
+++ app/js/dfa/bulkTesterDFA.js
@@ -10,12 +10,19 @@ function BulkTesterDFA($scope) {
 
     };
 
+    self.acceptedInput = [];
+    self.rejectedInput = [];
+
     self.testAcceptedInput = function () {
         var acceptedInputString = document.getElementById("acceptedInput").value;
         var acceptedInputArray = acceptedInputString.split("\n");
 
         _.forEach(acceptedInputArray, function (acceptedWord) {
-            console.log($scope.simulator.isInputWordAccepted(acceptedWord));
+            var tmpObj = {};
+            tmpObj.word = acceptedWord;
+            tmpObj.accepted = $scope.simulator.isInputWordAccepted(acceptedWord);
+            console.log();
+            self.acceptedInput.push(tmpObj);
         })
     };
 
@@ -24,7 +31,11 @@ function BulkTesterDFA($scope) {
         var rejectedInputArray = rejectedInputString.split("\n");
 
         _.forEach(rejectedInputArray, function (rejectedWord) {
-
+            var tmpObj = {};
+            tmpObj.word = rejectedWord;
+            tmpObj.rejected = !$scope.simulator.isInputWordAccepted(rejectedWord);
+            console.log();
+            self.rejectedInput.push(tmpObj);
         })
     };
 
