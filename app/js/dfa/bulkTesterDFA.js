a8bf9e9e3a4d2ffe5b0f1b65ca294502926426fa
MRisto <risto.matthias@gmail.com>
2016-08-18 04:31:25 +0200

    Update bulkTester

--- app/js/dfa/bulkTesterDFA.js
+++ app/js/dfa/bulkTesterDFA.js
@@ -1,28 +1,30 @@
 //This components is used for bulkTesting words
 function BulkTesterDFA($scope) {
     var self = this;
+    self.acceptedInput = [];
+    self.rejectedInput = [];
 
     self.bulkTest = function () {
+        self.acceptedInput = [];
+        self.rejectedInput = [];
         console.log("starting bulkTest");
         self.testAcceptedInput();
 
         self.testRejectedInput();
 
     };
-
-    self.acceptedInput = [];
-    self.rejectedInput = [];
-
     self.testAcceptedInput = function () {
         var acceptedInputString = document.getElementById("acceptedInput").value;
         var acceptedInputArray = acceptedInputString.split("\n");
 
         _.forEach(acceptedInputArray, function (acceptedWord) {
-            var tmpObj = {};
-            tmpObj.word = acceptedWord;
-            tmpObj.accepted = $scope.simulator.isInputWordAccepted(acceptedWord);
-            console.log();
-            self.acceptedInput.push(tmpObj);
+            if (acceptedWord !== "") {
+                var tmpObj = {};
+                tmpObj.word = acceptedWord;
+                tmpObj.accepted = $scope.simulator.isInputWordAccepted(acceptedWord);
+                console.log();
+                self.acceptedInput.push(tmpObj);
+            }
         })
     };
 
@@ -31,11 +33,13 @@ function BulkTesterDFA($scope) {
         var rejectedInputArray = rejectedInputString.split("\n");
 
         _.forEach(rejectedInputArray, function (rejectedWord) {
-            var tmpObj = {};
-            tmpObj.word = rejectedWord;
-            tmpObj.rejected = !$scope.simulator.isInputWordAccepted(rejectedWord);
-            console.log();
-            self.rejectedInput.push(tmpObj);
+            if (rejectedWord !== "") {
+                var tmpObj = {};
+                tmpObj.word = rejectedWord;
+                tmpObj.rejected = !$scope.simulator.isInputWordAccepted(rejectedWord);
+                console.log();
+                self.rejectedInput.push(tmpObj);
+            }
         })
     };
 
