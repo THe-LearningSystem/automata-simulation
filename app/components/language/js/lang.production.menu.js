4086619da57dc3d525ba8481c34532983de32d7b
Embe1 <beckermichael.herhahn.apple@googlemail.com>
2017-02-13 13:07:17 +0100

    Added "add Production Rule " in settings menu.

--- app/components/language/js/lang.production.menu.js
+++ app/components/language/js/lang.production.menu.js
@@ -2,5 +2,85 @@ autoSim.ProductionMenu = function ($scope) {
     var self = this;
 
     console.log("langProductionMenu");
+    
+    self.edit = {};
+    self.edit.isOpen = false;
+    self.edit.state = null;
+    self.edit.watcher = [];
 
+    self.edit.openHandler = function () {
+        event.preventDefault();
+        $scope.productions.menu.edit.open($scope.productions.getById(parseInt(d3.select(this).attr("object-id"))));
+    };
+
+    self.edit.open = function (production) {
+        $scope.core.closeMenus();
+        if (d3.event !== null && d3.event.stopPropagation !== undefined)
+            d3.event.stopPropagation();
+        $scope.productions.selected = production;
+        self.edit.production = _.cloneDeep(production);
+
+        self.edit.watcher.push($scope.$watch('productions.menu.edit.production.id', function (newValue, oldValue) {
+            if (newValue !== oldValue) {
+                if (newValue !== "" && newValue != undefined ) {
+                    !$scope.productions.rename($scope.productions.selected, newValue);
+                }
+            }
+        }));
+        self.edit.isOpen = true;
+
+        $scope.saveApply();
+    };
+
+    self.edit.close = function () {
+        _.forEach(self.edit.watcher, function (value) {
+            value();
+        });
+        self.edit.watcher = [];
+        self.edit.production = null;
+        $scope.productions.selected = null;
+        self.edit.isOpen = false;
+        $scope.saveApply();
+    };
+
+    self.context = {};
+    self.context.isOpen = false;
+    self.context.position = {};
+
+    self.context.openHandler = function () {
+        self.edit.close();
+        event.preventDefault();
+        self.context.open($scope.productions.getById(parseInt(d3.select(this).attr("object-id"))));
+
+    };
+
+    self.context.open = function (production) {
+        $scope.core.closeMenus();
+        if (d3.event !== null && d3.event.stopPropagation !== undefined)
+            d3.event.stopPropagation();
+
+        $scope.productions.selected = production;
+        self.context.position.x = event.layerX;
+        self.context.position.y = event.layerY;
+        self.context.isOpen = true;
+        $scope.saveApply();
+    };
+
+    self.context.close = function (dontRemoveSelectedState) {
+        self.context.position = {};
+        if (dontRemoveSelectedState !== true)
+            $scope.productions.selected = null;
+        self.context.isOpen = false;
+        $scope.saveApply();
+    };
+
+
+    self.close = function () {
+        self.context.close();
+        self.edit.close();
+    };
+
+    self.isOpen = function () {
+        return self.edit.isOpen || self.context.isOpen;
+    }
 };
--- app/components/language/js/lang.productions.js
+++ app/components/language/js/lang.productions.js
@@ -6,6 +6,7 @@ autoSim.Productions = function ($scope) {
     self.productionId = 0;
 
     self.create = function (prLeft, prRight) {
+        console.log("createProduction");
         return self.createWithId(self.productionId++, prLeft, prRight);
     };
     
@@ -14,5 +15,19 @@ autoSim.Productions = function ($scope) {
         self.push(production);
         return production;
     };
+    
+    self.existsWithName = function (productionLeft, productionRight, productionId) {
+        var tmp = false;
+        _.forEach(self, function (production) {
+            if (production.left == productionLeft && 
+                production.right == productionRight && 
+                ( stateId === undefined || state.id !== stateId)) {
+                tmp = true;
+                return false;
+            }
+        });
+        return tmp;
+    };
+    
 };
 autoSim.Productions.prototype = Array.prototype;
