c61bfecb7051321addc6c760ae361e519f4a87dd
Embe1 <beckermichael.herhahn.apple@googlemail.com>
2017-03-15 19:24:25 +0100

    First functional drawing of productions in derivationtree.

--- app/components/language/directives/svg/svg-Lang-Directives.js
+++ app/components/language/directives/svg/svg-Lang-Directives.js
@@ -5,7 +5,7 @@ autoSim.directive("ownlangsvg", function () {
         replace: true,
         link: function (scope, elm, attrs) {
             
-            scope.derivationtree.svg = d3.select('#diagram-svg');
+            scope.derivationtree.svg = d3.select('#diagram-lang-svg');
             elm.on('contextmenu', function () {
                 event.preventDefault();
                 if (!scope.derivationtree.menu.preventSvgContextClick) {
@@ -42,5 +42,38 @@ autoSim.directive("svglanggrid", function () {
 
         },
         templateUrl: 'components/language/directives/svg/svg-lang-grid.html'
+    };
+});
+
+autoSim.directive("svglangouter", function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        transclude: true,
+        templateNamespace: 'svg',
+        link: function (scope, elm, attrs) {
+            scope.derivationtree.svgLangOuter = d3.select("#diagram-lang-svg");
+            d3.select("#diagram-lang-svg").call(scope.derivationtree.zoom.behaviour);
+
+        },
+        templateUrl: 'components/language/directives/svg/svg-lang-outer.html'
+    };
+});
+
+autoSim.directive("svgtest", function () {
+    return {
+        replace: true,
+        restrict: 'E',
+        scope: {
+            production: '=production'
+        },
+        templateNamespace: 'svg',
+        link: function (scope, elm, attrs) {
+            var self = this;
+            scope.productions = scope.$parent.productions;
+            scope.simulator = scope.$parent.simulator;
+        }
+        ,
+        templateUrl: 'components/language/directives/svg/svg-Test.html'
     };
 });
